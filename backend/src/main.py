1e46aa4c9eb20c0557f50f3c58bfdff3ccc92e14
livingstone234 <seblelivingstone@gmail.com>
2026-03-15 02:55:02 +0000

    Updated the main.py file and added railway.json file

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 import os
 
-MY_PROJECT = os.environ.get("MY_PROJECT")
 
 app = FastAPI()
 
+MY_PROJECT = os.environ.get("MY_PROJECT")
+API_KEY = os.environ.get("API_KEY")
+if not API_KEY:
+    raise NotImplementedError("'API_KEY' was not set")
+
+
 @app.get("/")
 async def greet():
-    return {"say": "hello world!", "MY_PROJECT":MY_PROJECT}
+    return {"say": "hello world!", "MY_PROJECT":MY_PROJECT, "API_KEY":API_KEY}
