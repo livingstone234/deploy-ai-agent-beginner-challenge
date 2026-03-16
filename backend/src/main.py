8e5b520a35b71e3244716ced0b1759baaed5a970
livingstone234 <seblelivingstone@gmail.com>
2026-03-16 12:27:01 +0000

    Implemented a check on main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,8 +4,9 @@ import os
 
 app = FastAPI()
 
-MY_PROJECT = os.environ.get("MY_PROJECT")
+MY_PROJECT = os.environ.get("MY_PROJECT") or "This is my project"
 API_KEY = os.environ.get("API_KEY")
+
 if not API_KEY:
     raise NotImplementedError("'API_KEY' was not set")
 
